fbe2295c49ebd51c55ede58d3d5a1be9d046b508
rajkumar gupta <rajkumar.gupta731@gmail.com>
2026-05-26 13:25:24 +0545

    Add project files.

---
+++ BlazorEFDemo/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorEFDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorEFDemo.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
