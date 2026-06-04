d4bb0ee2475ddb0b377e7b8b0442df66747bea74
rajkumar gupta <rajkumar.gupta731@gmail.com>
2026-06-04 08:25:37 +0545

    Js interop demo

---
+++ BlazorEFDemo/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿function showMessage(message) {
+    alert(message);
+}
+
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text);
+} 
+
+async function getMessage() {
+
+    let result =
+        await DotNet.invokeMethodAsync(
+            'BlazorEFDemo',
+            'GetMessage');
+     
+    alert(result);
+}
+
+
+function saveData(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getData(key) {
+    return localStorage.getItem(key);
+}
